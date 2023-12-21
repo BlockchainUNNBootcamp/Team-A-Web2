2c8013cf2d52f6c25ab209b91a403e0250833c7d
divine richard <divinerichard007@gmail.com>
2023-12-21 13:00:00 -0800

    divines commit first ammended

---
+++ women.js
@@ -0,0 +1,28 @@
+
+function changeText(id) {
+    id.innerHTML = ['Wellness and Self-Care'];
+}
+function refreshText(id) {
+    id.innerHTML = "Women's Bags";
+}
+
+
+
+
+function changeTexts(id) {
+    id.innerHTML = ['Clean Products'];
+}
+function refreshTexts(id) {
+    id.innerHTML = "Guarantee on your products";
+}
+
+
+function changeTextss(id) {
+    id.innerHTML = ['Kiddies Swoosh'];
+}
+function refreshTextss(id) {
+    id.innerHTML = "Love, Kiddies";
+}
+
+
+
